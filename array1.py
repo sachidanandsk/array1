ddbb06f778f7e2a8810fe237bd05e475bf2d9d35
sachidanandsk <02fe24bca032@kletech.ac.in>
2026-01-03 09:03:53 +0530

    Add main function to calculate average of scores

---
+++ array1.py
@@ -0,0 +1,13 @@
+def main():
+    scores = [10, 20, 30, 50]
+    total = sum(scores)
+    average = total / len(scores)
+
+    print("=== main/master branch output ===")
+    print(f"Scores = {scores}")
+    print(f"Average = {average}")
+   
+  
+
+if __name__ == "__main__":
+    main()
